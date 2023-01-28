ddb65637ebef2afe1e7c04278d2c7d9dd3dd5e40
YannBcf <Yannbeats.pro@gmail.com>
2023-01-28 21:36:56 +0100

    feat(commands): rename slash command

--- src/commands/rss.ts
+++ src/commands/rss.ts
@@ -1,7 +1,9 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 import { Feed } from "../generator/feed";
 
-export const rssCommand = new SlashCommandBuilder().setName("ping").setDescription("lol");
+export const rssCommand = new SlashCommandBuilder()
+    .setName("rss")
+    .setDescription("Regenerates the rss file");
 
 export async function rssAction(interaction: ChatInputCommandInteraction): Promise<void> {
     try {
