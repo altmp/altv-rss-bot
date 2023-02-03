19193504ffb08eb5fbbe03b5072f38dfb3b7f333
YannBcf <Yannbeats.pro@gmail.com>
2023-02-03 12:29:33 +0100

    feat: add partials
    Allow the bot to react to the messages posted before it was started. This is needed because by default only the new messages are cached

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,9 @@
-import { REST, Client, GatewayIntentBits } from "discord.js";
+import { REST, Client, GatewayIntentBits, Partials } from "discord.js";
 import { config } from "./config";
 
 export const rest = new REST({ version: "10" }).setToken(config.bot.token);
 export const rssBot = new Client({
+    partials: [Partials.Channel, Partials.Message],
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
