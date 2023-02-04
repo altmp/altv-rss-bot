ce76da202af3fd1ebf53d20c6a069c75b66f9d7a
YannBcf <Yannbeats.pro@gmail.com>
2023-02-04 15:44:07 +0100

    feat: parse every type of timestamps

--- src/parser.ts
+++ src/parser.ts
@@ -8,11 +8,35 @@ const Regex = Object.freeze({
     R_ROLE: /(?<=\<span class="d-mention d-role">)(.*?)(?=<\/span>)/g,
     R_CHANNEL: /(?<=\<span class="d-mention d-channel">)(.*?)(?=<\/span>)/g,
     R_USER: /(?<=\<span class="d-mention d-user">)(.*?)(?=<\/span>)/g,
-    R_TIMESTAMP: /&lt;t:(.*?)(:F&gt;|:R&gt;|&gt;)/g,
+    R_TIMESTAMP: /<t:(\d+)(?:\:(\w))?>/g,
 });
 
 function hexToRgb(hex: string) {
     return `${hex.match(/\w\w/g)?.map((x) => +`0x${x}`)}`;
+}
+
+function transformTimestamps(content: string): string {
+    const timestampStyles = [undefined, "t", "T", "d", "D", "f", "F", "R"];
+
+    for (const [origin, timestamp, style] of content.matchAll(Regex.R_TIMESTAMP)) {
+        if (timestamp == null || !timestampStyles.includes(style)) {
+            console.error(`Skipped timestamp (${origin})`);
+            continue;
+        }
+
+        /**
+         * I choosed to not override the the default style to "f" so
+         * if Discord changes it in the future, its less likely to break
+         */
+        content = content.replace(
+            origin,
+            `<span ${
+                style == null ? "data-timestamp" : `data-timestamp="${style}"`
+            }>${timestamp}</span>`
+        );
+    }
+
+    return content;
 }
 
 function transform(regex: RegExp, content: string, callback: (id: string) => void) {
@@ -33,12 +57,29 @@ function transform(regex: RegExp, content: string, callback: (id: string) => voi
     }
 }
 
+function transformImages(content: string, attachments: IterableIterator<Attachment>): string {
+    let imgs = "";
+
+    for (const attachment of attachments) {
+        if (["jpg", "png"].includes(attachment.url.split(".")?.at(-1) ?? "")) {
+            imgs += `<img alt="${attachment.name}" src="${attachment.url}">`;
+        }
+    }
+
+    if (imgs.length > 0) {
+        content += `<span data-images>${imgs}</span>`;
+    }
+
+    return content;
+}
+
 export function parseContent(content: string, attachments: IterableIterator<Attachment>): string {
     const guild = rssBot.guilds.cache.get(config.discord.guild_id);
     if (!guild) {
         throw new Error(`No guild found for the id (${config.discord.guild_id})`);
     }
 
+    content = transformTimestamps(content);
     content = toHTML(content);
 
     transform(Regex.R_CHANNEL, content, (id) => {
@@ -73,13 +114,6 @@ export function parseContent(content: string, attachments: IterableIterator<Atta
         }
     });
 
-    for (const target of content.matchAll(Regex.R_TIMESTAMP)) {
-        const [selection, timestamp] = target;
-        if (timestamp) {
-            content = content.replaceAll(selection, `<span data-timestamp='T'>${timestamp}</span>`);
-        }
-    }
-
     // set @everyone and @here as mentions
     content = content.replaceAll("@everyone", "<span data-mention>@everyone</span>");
     content = content.replaceAll("@here", "<span data-mention>@here</span>");
@@ -88,16 +122,8 @@ export function parseContent(content: string, attachments: IterableIterator<Atta
     content = content.replaceAll('<img class="d-emoji d-emoji-animated"', "<img data-emoji");
     content = content.replaceAll('<img class="d-emoji"', "<img data-emoji");
 
-    let imgs = "";
-    for (const attachment of attachments) {
-        if (["jpg", "png"].includes(attachment.url.split(".")?.at(-1) ?? "")) {
-            imgs += `<img alt="${attachment.name}" src="${attachment.url}">`;
-        }
-    }
-
-    if (imgs.length > 0) {
-        content += `<span data-images>${imgs}</span>`;
-    }
+    // transform images at the end of the content
+    content = transformImages(content, attachments);
 
     return content;
 }
